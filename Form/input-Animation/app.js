cf66d12af883a5d2fadaa74fa66d88c8b545c8a8
engrmamun66 <engrmamun66@gmail.com>
2021-02-01 21:16:04 +0600

    d

---
+++ Form/input-Animation/app.js
@@ -0,0 +1,10 @@
+// const hamburger = document.querySelector(".hamburger");
+// const Navlinks = document.querySelector(".nav-links");
+// const links = document.querySelector(".nav-links li");
+
+// hamburger.addEventListener('click',  ()=>{
+//     Navlinks.classList.toggle("open");
+//     links.forEach(link => {
+//         link.classList.toggle('fade');
+//     });
+// })
